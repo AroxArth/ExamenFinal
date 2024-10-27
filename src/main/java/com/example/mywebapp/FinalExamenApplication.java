8534b2a3073b1427bea75128185ba4d5999da2fc
Aron Ayasta <I202313987@cibertec.edu.pe>
2024-10-27 10:13:01 -0500

    final

---
+++ src/main/java/com/example/mywebapp/FinalExamenApplication.java
@@ -0,0 +1,13 @@
+package com.example.mywebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalExamenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalExamenApplication.class, args);
+	}
+
+}
